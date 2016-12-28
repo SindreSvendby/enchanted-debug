ebae087614d97effa9008d01ad72d1dbf04458b2
Sindre Svendby <sinsvend@gmail.com>
2016-12-28 23:12:34 +0100

    2 test passed

--- src/index.js
+++ src/index.js
@@ -1,81 +1,118 @@
-import * as debugModule from 'debug';
+import debug from 'debug';
 import relativeFilepathFromModule from 'relative-filepath-from-module';
 import stack from 'callsite';
 
 const regexDeleteFilending = /\..*?$/;
 const regexFindForwardSlashes = /\//g;
 const regexDeleteIndexFromFilePath = /\/index$/;
+const log = debug('enchanted-debug:index');
 
-let globalTraceFunction = true;
-let globalTraceLine = true;
-let debug = debugModule;
+export default function enchantDebug() {
+	const errorQ = [];
+	const logQ = [];
+	let logInternal;
+	let errorInternal;
+	const callstack = stack();
+	const initObservers = [];
 
-export function setTraceLine(bool) {
-  globalTraceLine = bool;
-}
-
-export function setTraceFunction(bool) {
-  globalTraceFunction = bool;
-}
-
-export function setDebug(newDebug) {
-  debug = newDebug;
-}
-
-
-export default function enchantDebug({ traceFunction, traceLine } = {}) {
-  function getTraceFunction() {
-    return traceFunction || globalTraceFunction;
-  }
-
-  function getTraceLine() {
-    return traceLine || globalTraceLine;
-  }
-  const errorQ = [];
-  const logQ = [];
-  let logInternal;
-  let errorInternal;
-  const callstack = stack();
-
-  if (typeof callstack.length === 'number' && callstack.length > 1) {
-    const callee = callstack[1];
-    const modulePath = relativeFilepathFromModule(callee.getFileName());
-    modulePath
+	if (typeof callstack.length === 'number' && callstack.length > 1) {
+		const callee = callstack[1];
+		const modulePath = relativeFilepathFromModule(callee.getFileName());
+		modulePath
     .then(path => path.replace(regexDeleteFilending, ''))
     .then(path => path.replace(regexDeleteIndexFromFilePath, ''))
     .then(path => path.replace(regexFindForwardSlashes, ':'))
-    .then((path) => {
-      logInternal = debug(path);
-      errorInternal = debug(`${path}:error`);
-      // TODO: should some extra info about the delay be added?
-      logQ.forEach(message => logInternal(`[Delayed by enchanted-debug module] ${message}`));
-      errorQ.forEach(message => errorInternal(`[Delayed by enchanted-debug module] ${message}`));
-    })
-    .catch((err) => {
-      // TODO: should fetch stacktrace from message instead.
-      logInternal = debug('???');
-      errorInternal = debug('???:error');
-      debug('aurora-debug:error')('Failed finding path to file, reason', err);
-      // TODO: should some extra info about the delay be added?
-      logQ.forEach(message => logInternal(message));
-      errorQ.forEach(message => errorInternal(message));
-    });
-  }
+    .then(configureDebug)
+		.catch(handleError);
+	}
 
-  return {
-    log: function logWrapper(message) {
-      if (typeof logInternal === 'function') {
-        logInternal(message);
-      } else {
-        logQ.push(message);
-      }
-    },
-    error: function errorWrapper(message) {
-      if (typeof errorInternal === 'function') {
-        errorInternal(message);
-      } else {
-        errorQ.push(message);
-      }
-    },
-  };
+	return {
+		log: logInternalWrapper,
+		error: errorInternalWrapper,
+		logLogger: logLogger,
+		isLogEmpty,
+		isErrorEmpty,
+		notifyOnInit
+	};
+
+	function logInternalWrapper(message) {
+		if (typeof logInternal === 'function') {
+			logLogger(message);
+		} else {
+			logQ.push(message);
+		}
+	}
+
+	function logLogger(message) {
+		logInternal(message);
+	}
+
+	function errorLogger(message) {
+		errorLogger(message);
+	}
+
+/**
+ * Send in and obeserver, will be called with method 'notify' on complete
+ */
+	function notifyOnInit(observer) {
+		initObservers.push(observer);
+	}
+
+	function errorInternalWrapper(message) {
+		if (typeof errorInternal === 'function') {
+			errorInternal(message);
+		} else {
+			errorQ.push(message);
+		}
+	}
+
+	function isLogEmpty() {
+		return logQ.length === 0;
+	}
+
+	function isErrorEmpty() {
+		return errorQ.length === 0;
+	}
+
+	function handleError(err) {
+		// log('handleError, error ', err);
+		// debug('enchanted-debug:error')('Failed finding path to file, reason:', err);
+		// TODO: should fetch stacktrace from message instead.
+		logInternal = debug('[enchanted-debug failed]');
+		errorInternal = debug('[enchanted-debug failed]:error');
+		printDelayedQueue(logQ, logLogger);
+		printDelayedQueue(errorQ, errorLogger);
+		log(err);
+		initObservers.forEach(observer => observer.notify());
+	}
+
+	function configureDebug(path) {
+		// log('configureDebug, setting path', path);
+		logInternal = debug(path);
+		errorInternal = debug(`${path}:error`);
+		printDelayedQueue(logQ, logLogger);
+		printDelayedQueue(errorQ, errorLogger);
+		initObservers.forEach(observer => {
+			// log('Notifying observers');
+			observer.notify();
+		});
+	}
+
+	function printDelayedQueue(queue, logger) {
+		// if (queue.length > 0) {
+		// 	logger('[Delayed messages by enchanted-debug module - start]');
+		// }
+		queue.forEach(message => logger(message));
+		// if (queue.length > 0) {
+		// 	logger('[Delayed messages by enchanted-debug module - stop]');
+		// }
+	}
+
+	function getTraceFunction() {
+		return traceFunction || globalTraceFunction;
+	}
+
+	function getTraceLine() {
+		return traceLine || globalTraceLine;
+	}
 }
--- test/test.js
+++ test/test.js
@@ -1,14 +1,50 @@
-import enchantDebug from '../src';
+import test from 'ava';
+import sinon from 'sinon';
+import eDebug from '../src';
+import debug from 'debug';
 
-const { log, error } = enchantDebug();
+// const log = debug('enchanted-debug:actual');
 
-function logHelloWorld(skipHelloWorld = false) {
-  log('Starting helloWorld');
-  if (skipHelloWorld) error('We are skipping logging of helloWorld, thats kind of strange?');
-  else log('Hello World!');
-  log('Finished helloWorld');
-  setTimeout(() => log('Finished helloWorld'), 100);
+test.before(t => {
+	sinon.spy(debug, 'log');
+});
+
+test.beforeEach(t => {
+	t.context.eDebug = eDebug();
+});
+
+// runs serial because I'm mocking out debug.log, not sure how make this local
+test.serial.cb('Automaticly added path to debug', t => {
+	const eLogger = t.context.eDebug;
+
+	const observer = createTestObserver(() => {
+		const message = debug.log.getCall(0).args[0];
+  	const actual = message.split(' ')[6];
+
+		t.is(actual, 'enchanted-debug:test:test');
+		t.end();
+	});
+
+	eLogger.notifyOnInit(observer);
+	eLogger.log('testing enchanted-debug');
+});
+
+test.cb('Message is passed true correctly ', t => {
+	const eLogger = t.context.eDebug;
+
+	const observer = createTestObserver(() => {
+		const message = debug.log.getCall(0).args[0];
+		const actual = message.split(' ').slice(7).join(' ');
+		t.is(actual, 'testing enchanted-debug');
+		t.end();
+	});
+
+	eLogger.notifyOnInit(observer);
+	eLogger.log('testing enchanted-debug');
+});
+
+function createTestObserver(onNotify) {
+	return {
+		notify: onNotify
+	};
 }
-
-logHelloWorld();
-logHelloWorld(true);
