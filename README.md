a9ae6a28892a7809e354771fae50e01b8a0ea3c2
Sindre Svendby <sinsvend@gmail.com>
2016-12-31 21:59:01 +0100

    rewrite

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-#Enchanted Debug
+#enchanted-debug
 
 
-a wrapper around the amazing (debug package)[https://www.npmjs.com/package/debug] that follow what we think is best practises
+a wrapper around the amazing [debug package](https://www.npmjs.com/package/debug) that follow what we think is best practises
+(this is while debug has not been released version 3, in version 3 they are adding middleware)
 
 ## cons
 1. Performance is not the best, since we use callsite
@@ -12,44 +13,56 @@ a wrapper around the amazing (debug package)[https://www.npmjs.com/package/debug
 
 ## Prerequisite
 
-Debug is a peerDependecy of `aurora-debug` and you can need to specified this your self
+enchanted-debug is a peerDependecy of `debug` and you can need to specified this your self
 
-So install `debug` => ( `yarn add debug` / `npm install --save-exact debug` )
+So install `debug`
+
+`yarn add debug` / `npm install --save-exact debug`
 
 ## Installation
 
-Install this package: `yarn add aurora-debug` or `npm install --save-exact aurora-debug`
+`yarn add aurora-debug` or `npm install --save-exact aurora-debug`
 
 ## Usage
 
+```js
+// assume this  file is under <project-root>/src/services/fetch/index.js
+import eDebug from 'enchanted-debug'
+// project root folder name, for this project
+const { log, error } from eDebug('<project-root>')
+
+log('starting')
+errror('failed')
+log('finished')
+
 ```
-import { log, error } from 'aurora-debug'
 
-
+Output from debug module will be
+```
+// output will be:
+<project-root>:src:services:fetch starting
+<project-root>:error:src:services:fetch: failed
+<project-root>:src:services:fetch finished
 ```
 
 This would be the same as doing this with plain debug:
-```
+```js
 import debug from 'debug';
 
 const path = '<project-root>:<subfolder>:<subfolder>:<subfolder>:<filename-without-ending>'
 const log = debug(`${path}`);
 const error = debug(`${path}:error`);
 
-function logHelloWorld(skipHelloWorld = false) {
-  log('Starting helloWorld');
-  if (skipHelloWorld) error('We are skipping logging of helloWorld, thats kind of strange?');
-  else log('Hello World!');
-  log('Finished helloWorld');
-}
-
-logHelloWorld();
-logHelloWorld(true);
-
-
-
+log()
 ```
 
+## Good to know
+
+skip ending:
+enchanted-debug/src/services/fetch/utils.js =>  enchanted-debug:src:services:fetch:utils
+
+skip index from last file
+enchanted-debug/src/services/fetch/index.js =>  enchanted-debug:src:services:fetch
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,118 +1,47 @@
 import debug from 'debug';
-import relativeFilepathFromModule from 'relative-filepath-from-module';
 import stack from 'callsite';
+import getPath from './get-path';
 
 const regexDeleteFilending = /\..*?$/;
 const regexFindForwardSlashes = /\//g;
 const regexDeleteIndexFromFilePath = /\/index$/;
-const log = debug('enchanted-debug:index');
 
-export default function enchantDebug() {
-	const errorQ = [];
-	const logQ = [];
+export default function enchantDebug(projectRootFolder = '') {
 	let logInternal;
 	let errorInternal;
 	const callstack = stack();
-	const initObservers = [];
 
 	if (typeof callstack.length === 'number' && callstack.length > 1) {
 		const callee = callstack[1];
-		const modulePath = relativeFilepathFromModule(callee.getFileName());
-		modulePath
-    .then(path => path.replace(regexDeleteFilending, ''))
-    .then(path => path.replace(regexDeleteIndexFromFilePath, ''))
-    .then(path => path.replace(regexFindForwardSlashes, ':'))
-    .then(configureDebug)
-		.catch(handleError);
+		const modulePath = getPath(projectRootFolder, callee.getFileName());
+		const debugNamespace = getNameSpace(modulePath);
+		const subPath = getSubNamespace(projectRootFolder, debugNamespace);
+		logInternal = debug(debugNamespace);
+		errorInternal = debug(`${projectRootFolder}:error:${subPath}`);
+		return {
+			log: logInternal,
+			error: errorInternal
+		};
 	}
 
 	return {
-		log: logInternalWrapper,
-		error: errorInternalWrapper,
-		logLogger: logLogger,
-		isLogEmpty,
-		isErrorEmpty,
-		notifyOnInit
+		log: debug(`${projectRootFolder}:[unkown]`),
+		error: debug(`${projectRootFolder}:error:[unkown]`)
 	};
-
-	function logInternalWrapper(message) {
-		if (typeof logInternal === 'function') {
-			logLogger(message);
-		} else {
-			logQ.push(message);
-		}
-	}
-
-	function logLogger(message) {
-		logInternal(message);
-	}
-
-	function errorLogger(message) {
-		errorLogger(message);
-	}
-
-/**
- * Send in and obeserver, will be called with method 'notify' on complete
- */
-	function notifyOnInit(observer) {
-		initObservers.push(observer);
-	}
-
-	function errorInternalWrapper(message) {
-		if (typeof errorInternal === 'function') {
-			errorInternal(message);
-		} else {
-			errorQ.push(message);
-		}
-	}
-
-	function isLogEmpty() {
-		return logQ.length === 0;
-	}
-
-	function isErrorEmpty() {
-		return errorQ.length === 0;
-	}
-
-	function handleError(err) {
-		// log('handleError, error ', err);
-		// debug('enchanted-debug:error')('Failed finding path to file, reason:', err);
-		// TODO: should fetch stacktrace from message instead.
-		logInternal = debug('[enchanted-debug failed]');
-		errorInternal = debug('[enchanted-debug failed]:error');
-		printDelayedQueue(logQ, logLogger);
-		printDelayedQueue(errorQ, errorLogger);
-		log(err);
-		initObservers.forEach(observer => observer.notify());
-	}
-
-	function configureDebug(path) {
-		// log('configureDebug, setting path', path);
-		logInternal = debug(path);
-		errorInternal = debug(`${path}:error`);
-		printDelayedQueue(logQ, logLogger);
-		printDelayedQueue(errorQ, errorLogger);
-		initObservers.forEach(observer => {
-			// log('Notifying observers');
-			observer.notify();
-		});
-	}
-
-	function printDelayedQueue(queue, logger) {
-		// if (queue.length > 0) {
-		// 	logger('[Delayed messages by enchanted-debug module - start]');
-		// }
-		queue.forEach(message => logger(message));
-		// if (queue.length > 0) {
-		// 	logger('[Delayed messages by enchanted-debug module - stop]');
-		// }
-	}
-
-	function getTraceFunction() {
-		return traceFunction || globalTraceFunction;
-	}
-
-	function getTraceLine() {
-		return traceLine || globalTraceLine;
-	}
 }
+
+function getNameSpace(modulePath) {
+	return modulePath
+    .replace(regexDeleteFilending, '')
+    .replace(regexDeleteIndexFromFilePath, '')
+    .replace(regexFindForwardSlashes, ':');
+}
+
+function getSubNamespace(projectRootFolder, debugNamespace) {
+	return debugNamespace.substr(projectRootFolder.length + 1);
+}
+
+export const testInternals = {
+	getSubNamespace,
+	getNameSpace
+};
